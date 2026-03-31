648dd392c1430ee77f313ed6f484d4d20319d06f
Robert Orchard <rjorchard2006@gmail.com>
2026-03-30 21:42:03 -0300

    Remove old jump system
    Was an interesting design, but too clunky in practice

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -25,7 +25,6 @@ public class PlayerControl : MonoBehaviour
     private Global global = null;
 
     Vector3 lastVelocity = Vector3.zero;
-    Transform jumpFace = null;
     bool onGround = true;
     int digits = 0; // how many digits in the volume when measured from grams (kg / 100)
 
@@ -73,25 +72,13 @@ public class PlayerControl : MonoBehaviour
 
         if (!onGround) moveInput *= airControlMult;
 
-        //Vector3 baseForce = moveForce * Time.fixedDeltaTime * -Vector3.up;
-
         if (moveInput.x != 0f) // left/right
         {
-            /*Vector3 _force = -Mathf.Abs(moveInput.x) * baseForce * rb.mass;
-            Vector3 _position = transform.position +
-                furthestEdgeDist * (Global.Instance.GlobalRight * (moveInput.x < 0f ? 1f : -1f));
-            rb.AddForceAtPosition(_force, _position, ForceMode.Force); // creates a torque*/
-
             Vector3 force = moveInput.x * moveForce * rb.mass * Time.fixedDeltaTime * global.GlobalRight;
             rb.AddForce(force, ForceMode.Force);
         }
         if (moveInput.y != 0f) // forward/backward
         {
-            /*Vector3 _force = -Mathf.Abs(moveInput.y) * baseForce * rb.mass;
-            Vector3 _position = transform.position +
-                furthestEdgeDist * (Global.Instance.GlobalForward * (moveInput.y < 0f ? 1f : -1f));
-            rb.AddForceAtPosition(_force, _position, ForceMode.Force);*/
-
             Vector3 force = moveInput.y * moveForce * rb.mass * Time.fixedDeltaTime * global.GlobalForward;
             rb.AddForce(force, ForceMode.Force);
         }
@@ -105,13 +92,10 @@ public class PlayerControl : MonoBehaviour
             return;
         }
 
-        if(jumpFace == null) return;
         if (!onGround) return;
         if(!input.Player.Jump.WasPressedThisFrame()) return;
 
-        Vector3 upDir = (jumpFace.position - transform.position).normalized;
-        upDir = Vector3.up;
-        rb.AddForce(rb.mass * (digits + baseJumpForce) * upDir, ForceMode.Impulse);
+        rb.AddForce(rb.mass * (digits + baseJumpForce) * Vector3.up, ForceMode.Impulse);
 
         elapsedCTime = maxCoyoteTime;
         elapsedSinceJump = 0f;
@@ -138,42 +122,12 @@ public class PlayerControl : MonoBehaviour
     {
         if (global == null || global.IsPaused) return;
 
-        CalculateJumpFace();
         Jump();
         Look();
 
         GroundCheck();
     }
 
-    // Finds best face to use in jump calculation
-    void CalculateJumpFace()
-    {
-        Vector3 targetPos = transform.position;
-        targetPos += lastMoveInput.x * .5f * global.GlobalRight;
-        targetPos += lastMoveInput.y * .5f * global.GlobalForward;
-        //targetPos += new Vector3(lastMoveInput.x, 0f, lastMoveInput.y) * .5f; // doesn't use cameraspace
-        targetPos.y += .3f;
-
-        int bestIndex = -1;
-        float lowestDist = Mathf.Infinity;
-
-        for(int i = 0; i < faces.Count; i++)
-        {
-            float distance = Vector3.Magnitude(targetPos - faces[i].position);
-            if(distance < lowestDist)
-            {
-                lowestDist = distance;
-                bestIndex = i;
-            }
-        }
-
-        if (bestIndex < 0) return;
-        //Debug.Log("Best Face Score " + lowestDist, faces[bestIndex].gameObject);
-
-        jumpFace = faces[bestIndex];
-    }
-
-    Vector3 debugPos = Vector3.zero;
     float furthestEdgeDist;
     void GroundCheck()
     {
@@ -194,8 +148,6 @@ public class PlayerControl : MonoBehaviour
         {
             onGround = false;
         }
-
-        debugPos = endPos;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -224,16 +176,9 @@ public class PlayerControl : MonoBehaviour
         furthestEdgeDist = (offset1 + offset2 + offset3).magnitude * 1.1f; // dist from center to a corner
     }
 
-    /*// for debug drawing
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(transform.position, debugPos);
-    }*/
-
     public void CalculateDigits(float valInMetersCubed)
     {
         digits = Mathf.FloorToInt(Mathf.Log10(valInMetersCubed * 100f) + 1);
-        Debug.Log(digits);
     }
     #endregion
 }
