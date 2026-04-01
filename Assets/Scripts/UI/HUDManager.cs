ef19a8bbae8c5f8c2cf36c87d983c876ac40b53d
Robert Orchard <rjorchard2006@gmail.com>
2026-03-31 22:15:09 -0300

    Update hud sprites
    heading my ups display

--- Assets/Scripts/UI/HUDManager.cs
+++ Assets/Scripts/UI/HUDManager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDManager : MonoBehaviour
 {
     public static HUDManager Instance = null;
 
     [SerializeField] public HUDTimer timer;
+    [SerializeField] Image boxfillImage;
     public float mapDuration = 600f;
 
     [SerializeField] TMP_Text volumeText;
@@ -22,6 +24,8 @@ public class HUDManager : MonoBehaviour
         Instance = this;
 
         StartCoroutine(DelayedStart());
+
+        boxfillImage.fillAmount = 0f;
     }
 
     IEnumerator DelayedStart()
@@ -35,5 +39,12 @@ public class HUDManager : MonoBehaviour
     public void UpdateVolume(float value)
     {
         volumeText.text = value.ToString() + " m<sup>3";
+
+        if(boxfillImage.fillAmount < 1f && Global.Instance != null && Global.Instance.levelData != null)
+        {
+            float _val = value / Global.Instance.levelData.targetVolume;
+            if(_val > 1f) _val = 1f;
+            boxfillImage.fillAmount = _val;
+        }
     }
 }
